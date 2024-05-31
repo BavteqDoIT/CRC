d636d546b20193f5aceb29f396e45063acb056d2
BavteqDoIT <125410325+BavteqDoIT@users.noreply.github.com>
2024-05-31 14:14:56 +0200

    Zaktualizowane o zmiany zasugerowane od Maćka

--- models.py
+++ models.py
@@ -1,26 +1,24 @@
-from sqlalchemy import Boolean, Column, ForeignKey, Integer, String
+from sqlalchemy import Boolean, ForeignKey, Integer, String
 from sqlalchemy.orm import relationship, mapped_column
 
-from    database import Base
-
+from database import Base
 
 class User(Base):
     __tablename__ = "users"
 
-    id = Column(Integer, primary_key=True)
+    id = mapped_column(Integer, primary_key=True, index=True)
     email = mapped_column(String, unique=True, index=True)
-    hashed_password = Column(String)
-    is_active = Column(Boolean, default=True)
+    hashed_password = mapped_column(String)
+    is_active = mapped_column(Boolean, default=True)
 
     items = relationship("Item", back_populates="owner")
-
 
 class Item(Base):
     __tablename__ = "items"
 
-    id = Column(Integer, primary_key=True)
-    title = Column(String, index=True)
-    description = Column(String, index=True)
-    owner_id = Column(Integer, ForeignKey("users.id"))
+    id = mapped_column(Integer, primary_key=True, index=True)
+    title = mapped_column(String, index=True)
+    description = mapped_column(String, index=True)
+    owner_id = mapped_column(Integer, ForeignKey("users.id"))
 
     owner = relationship("User", back_populates="items")
